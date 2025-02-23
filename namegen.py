da2e123929265aac47a73f04691b81a2a51dc5ee
Luciano Ramalho <luciano@ramalho.org>
2025-02-23 17:22:27 -0300

    gerar nomes por Cadeia de Markov

---
+++ namegen.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+import pickle
+import random
+import sys
+
+from collections import defaultdict
+from collections.abc import Iterable, Mapping
+from pathlib import Path
+
+MarkovModel = Mapping[str, list[str]]
+Starters = list[str]
+
+MODEL_PATH = Path('markov-model.pickle')
+CONJUNCOES = "da das de di do dos du del von van sant"
+CONJUNCOES += " " + CONJUNCOES.title()
+CONJUNCOES = set(CONJUNCOES.split())
+
+
+class MarkovNameMaker:
+    def __init__(self, names:Iterable[str], order:int=6):
+        self.order = order
+        self.end_char = ''
+        if MODEL_PATH.exists():
+            with open(MODEL_PATH, 'rb') as f:
+                self.model, self.starters = pickle.load(f)
+        else:
+            sys.stderr.write(f'Building model {MODEL_PATH}...\n')
+            self.model, self.starters = self._build_model(names)
+            with open(MODEL_PATH, 'wb') as f:
+                pickle.dump((self.model, self.starters), f)
+
+    def _build_model(self, names:Iterable[str]) -> tuple[MarkovModel, Starters]:
+        model:MarkovModel = defaultdict(list)
+        starters:Starters = []
+
+        for name in names:
+            if self.end_char == '':
+                self.end_char = name[-1]
+            else:
+                if self.end_char != name[-1]:
+                    print('All names must have the same last character. Ex: \\n')
+                    sys.exit(1)
+            starters.append(name[:self.order])
+            for i in range(len(name) - self.order):
+                key = name[i:i + self.order]
+                next_char = name[i + self.order]
+                model[key].append(next_char)
+        return model, starters
+
+    def make_name(self, max_length:int=80):
+        name = random.choice(self.starters)
+        while len(name) < max_length:
+            key = name[-self.order:]
+            if key not in self.model:
+                break
+            next_char = random.choice(self.model[key])
+            parts = name.split()
+            if next_char == self.end_char:
+                if len(parts) >= 4:
+                    break
+                else:
+                    next_char = ' '
+            elif (
+                next_char == ' '
+                and parts[-1] not in CONJUNCOES
+                and len(parts[-1]) > 1
+                and len(parts) >= 4):
+                break
+            name += next_char
+        return name.strip()
+
+def make_names(names_file_path, quantity, order):
+    with open(names_file_path) as f:
+        names = f.readlines()
+    maker = MarkovNameMaker(names, order)
+    for _ in range(quantity):
+        print(maker.make_name())
+
+if __name__ == '__main__':
+    quantity = int(sys.argv[1])
+    make_names('amostras/nomes.txt', quantity, 6)
